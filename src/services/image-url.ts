49ff74c530c36ae05252d3e4b5e1602616a36294
Lobsang <lobsangphun20@gmail.com>
2026-04-28 15:23:38 +0100

    no image placeholder

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,8 @@
+import noImage from "../assets/no-image.webp";
+
 const getCroppedImageUrl = (url: string) => {
 
-  if (!url) return "";
+  if (!url) return noImage;
 
     const target = "media/";
     const index = url.indexOf(target) + target.length; // i.e. from starting to the end of "media/"
