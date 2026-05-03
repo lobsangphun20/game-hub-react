e3c1bfbf968a4894a9e0f72827828f9126793759
Lobsang <lobsangphun20@gmail.com>
2026-05-03 15:38:23 +0100

    Static genre refractor

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -18,9 +18,9 @@ interface Props {
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenre();
 
-  if (isLoading) return <Spinner />;
+  // if (isLoading) return <Spinner />; *****No longer needed since we are using static data, but if we were fetching from an API, we would want to show a loading spinner while the data is being fetched. This is because fetching data from an API can take some time, and we want to provide feedback to the user that something is happening. The spinner indicates that the data is being loaded and will be displayed once it is ready. In this case, since we are using static data, there is no need for a loading state, so we can remove the spinner.*****
 
-  if (error) return null;
+  // if (error) return null;
 
   return (
     <>
